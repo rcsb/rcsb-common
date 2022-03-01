3ad237ac18ea8a83cb7b00cd4476a0a6abe888e9
valasatava <yana.valasatava@gmail.com>
2022-02-28 18:12:43 -0800

    combined index for groups

--- src/main/java/org/rcsb/common/constants/ElasticsearchConstants.java
+++ src/main/java/org/rcsb/common/constants/ElasticsearchConstants.java
@@ -27,6 +27,7 @@ public class ElasticsearchConstants {
     public static final String INDEX_UNRELEASED_ENTRY = "unreleased_entry";
     public static final String INDEX_UNSTRUCTURED_ENTRY = "unstructured_entry";
 
+    public static final String INDEX_DENORMALIZED_GROUP = "combined_group";
     public static final String INDEX_GROUP_POLYMER_ENTITY_UNIPROT_ACCESSION = "group_polymer_entity_uniprot_accession";
     public static final String INDEX_GROUP_POLYMER_ENTITY_SEQUENCE_IDENTITY = "group_polymer_entity_sequence_identity";
     public static final String INDEX_GROUP_ENTRY_DEPOSIT_GROUP = "group_entry_deposit_group";
