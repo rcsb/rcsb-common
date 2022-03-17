3b9db6b58a86c913d9d79898e94d9ee8344cd650
Yana Rose <yana.valasatava@gmail.com>
2022-03-17 15:50:47 -0700

    content type enum that can be shared between projects

---
+++ src/main/java/org/rcsb/common/constants/ContentType.java
@@ -0,0 +1,17 @@
+package org.rcsb.common.constants;
+
+/**
+ * Lists the content types that are allowed or denied by loading process
+ * that populates ExchangeDB and DW, e.g. experimental or computed structure
+ * models.
+ *
+ * Created on 3/17/22.
+ * TODO: fix @since tag
+ *
+ * @author Yana Rose
+ * @since X.Y.Z
+ */
+public enum ContentType {
+    experimental,
+    computed
+}
