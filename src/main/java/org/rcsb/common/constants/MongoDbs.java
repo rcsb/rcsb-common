2d1af5c1d1cd60174d63e4de926ffdda2dd21d10
josemduarte <jose.m.duarte@gmail.com>
2025-11-04 10:50:53 -0800

    Adding db name constants

---
+++ src/main/java/org/rcsb/common/constants/MongoDbs.java
@@ -0,0 +1,9 @@
+package org.rcsb.common.constants;
+
+public class MongoDbs {
+
+    public static final String DW_DB = "dw";
+    public static final String CORE_PDB_DB = "pdbx_core";
+    public static final String CORE_CSM_DB = "pdbx_comp_model_core";
+
+}
