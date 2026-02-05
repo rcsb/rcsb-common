ee2c070782c95dfe960c522c8952cf22708d5c38
josemduarte <jose.m.duarte@gmail.com>
2026-02-05 15:09:20 -0800

    Adding getContentTypes

--- src/main/java/org/rcsb/common/constants/LoadPdbCsmType.java
+++ src/main/java/org/rcsb/common/constants/LoadPdbCsmType.java
@@ -1,8 +1,20 @@
 package org.rcsb.common.constants;
 
+import java.util.List;
+
 public enum LoadPdbCsmType {
-    PDB,
-    CSM,
-    ALL;
+
+    PDB(ContentType.experimental),
+    CSM(ContentType.computational),
+    ALL(ContentType.experimental, ContentType.computational);
+
+    private final List<ContentType> contentTypes;
+    LoadPdbCsmType(ContentType... contentTypes) {
+        this.contentTypes = List.of(contentTypes);
+    }
+
+    public List<ContentType> getContentTypes() {
+        return contentTypes;
+    }
 
 }
