435ee2f67c6bcab7501507aaa28314fa8b0bf630
ChunxiaoB <chunxiao.bi@rcsb.org>
2026-02-10 14:25:31 -0800

    remove unused isPdbIdWithSuffix method

--- src/main/java/org/rcsb/common/constants/IdentifierRegex.java
+++ src/main/java/org/rcsb/common/constants/IdentifierRegex.java
@@ -5,6 +5,7 @@ import java.util.regex.Pattern;
 public class IdentifierRegex {
 
     public static final Pattern PDB_ID_REGEX = Pattern.compile("^\\d\\w\\w\\w$");
+
     private static final String PDB_ID_WITH_SUFFIX_REGEX_VALUE = String.format(
             "^\\d\\w\\w\\w(?:%s\\w+|%s\\w+|%s\\w+)$",
             Pattern.quote(IdentifierSeparator.ENTITY_SEPARATOR),
@@ -12,9 +13,4 @@ public class IdentifierRegex {
             Pattern.quote(IdentifierSeparator.ENTITY_INSTANCE_SEPARATOR));
 
     public static final Pattern PDB_ID_WITH_SUFFIX_REGEX = Pattern.compile(PDB_ID_WITH_SUFFIX_REGEX_VALUE);
-
-    public static boolean isPdbIdWithSuffix(String identifier) {
-        return identifier != null && PDB_ID_WITH_SUFFIX_REGEX.matcher(identifier).matches();
-    }
-    
 }
