c3c43e677f05003e49c702a958e3c354a424cf3c
ChunxiaoB <chunxiao.bi@rcsb.org>
2026-02-13 17:19:30 -0800

    add unit tests for IdentifierRegex.PDB_ID_REGEX

---
+++ src/test/java/org/rcsb/common/constants/IdentifierRegexTest.java
@@ -0,0 +1,20 @@
+package org.rcsb.common.constants;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class IdentifierRegexTest {
+
+    @Test
+    public void matchesFourCharacterPdbId() {
+        assertTrue(IdentifierRegex.PDB_ID_REGEX.matcher("1abc").matches());
+    }
+
+    @Test
+    public void matchesFourCharacterPdbIdWithValidSuffix() {
+        assertTrue(IdentifierRegex.PDB_ID_REGEX.matcher("1abc" + IdentifierSeparator.ENTITY_SEPARATOR + "2").matches());
+        assertTrue(IdentifierRegex.PDB_ID_REGEX.matcher("1abc" + IdentifierSeparator.ASSEMBLY_SEPARATOR + "1").matches());
+        assertTrue(IdentifierRegex.PDB_ID_REGEX.matcher("1abc" + IdentifierSeparator.ENTITY_INSTANCE_SEPARATOR + "A").matches());
+    }
+}
