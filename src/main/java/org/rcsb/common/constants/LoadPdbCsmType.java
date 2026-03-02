b3e7eb9157b2f953c073ef01b4b0649c21529cf9
josemduarte <jose.m.duarte@gmail.com>
2026-03-02 15:35:38 -0800

    Removing unneeded field in LoadPdbCsmType

--- src/main/java/org/rcsb/common/constants/LoadPdbCsmType.java
+++ src/main/java/org/rcsb/common/constants/LoadPdbCsmType.java
@@ -1,20 +1,9 @@
 package org.rcsb.common.constants;
 
-import java.util.List;
 
 public enum LoadPdbCsmType {
 
-    PDB(ContentType.experimental),
-    CSM(ContentType.computational),
-    ALL(ContentType.experimental, ContentType.computational);
-
-    private final List<ContentType> contentTypes;
-    LoadPdbCsmType(ContentType... contentTypes) {
-        this.contentTypes = List.of(contentTypes);
-    }
-
-    public List<ContentType> getContentTypes() {
-        return contentTypes;
-    }
-
+    PDB,
+    CSM,
+    ALL;
 }
