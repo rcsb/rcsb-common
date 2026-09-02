db2614036d9bd3e3805d3407bdc54400ad1bb80b
Jose Duarte <jose.m.duarte@gmail.com>
2026-09-01 18:08:26 -0700

    Adding ext-id regexes

--- src/main/java/org/rcsb/common/constants/IdentifierRegex.java
+++ src/main/java/org/rcsb/common/constants/IdentifierRegex.java
@@ -4,14 +4,27 @@ import java.util.regex.Pattern;
 
 public class IdentifierRegex {
 
-    public static final Pattern PDB_ID_REGEX = Pattern.compile("^\\d\\w\\w\\w$");
+    private static final String ALPHANUM_VALUE = "[a-zA-Z0-9]";
 
-    private static final String PDB_ID_WITH_SUFFIX_REGEX_VALUE = String.format(
-            "^\\d\\w\\w\\w(?:%s\\w+(?:%s\\w+)?|%s\\w+|%s\\w+)?$",
+    private static final String PDB_ID_REGEX_VALUE = "\\d" + ALPHANUM_VALUE + "{3}";
+
+    private static final String EXT_PDB_ID_REGEX_VALUE = "pdb_[a-z0-9]{8}";
+
+    private static final String SUFFIX_REGEX_VALUE = String.format(
+            "(?:%1$s%5$s+(?:%2$s%5$s+)?|%3$s%5$s+|%4$s%5$s+)?",
             Pattern.quote(IdentifierSeparator.ASSEMBLY_SEPARATOR),
             Pattern.quote(IdentifierSeparator.INTERFACE_SEPARATOR),
             Pattern.quote(IdentifierSeparator.ENTITY_SEPARATOR),
-            Pattern.quote(IdentifierSeparator.ENTITY_INSTANCE_SEPARATOR));
+            Pattern.quote(IdentifierSeparator.ENTITY_INSTANCE_SEPARATOR),
+            ALPHANUM_VALUE);
 
-    public static final Pattern PDB_ID_WITH_SUFFIX_REGEX = Pattern.compile(PDB_ID_WITH_SUFFIX_REGEX_VALUE);
+    public static final Pattern PDB_ID_REGEX = Pattern.compile("^" + PDB_ID_REGEX_VALUE + "$");
+
+    public static final Pattern EXT_PDB_ID_REGEX = Pattern.compile("^" + EXT_PDB_ID_REGEX_VALUE + "$");
+
+    public static final Pattern PDB_ID_WITH_SUFFIX_REGEX =
+            Pattern.compile("^" + PDB_ID_REGEX_VALUE + SUFFIX_REGEX_VALUE + "$");
+
+    public static final Pattern EXT_PDB_ID_WITH_SUFFIX_REGEX =
+            Pattern.compile("^" + EXT_PDB_ID_REGEX_VALUE + SUFFIX_REGEX_VALUE + "$");
 }
